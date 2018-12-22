cfff8c1fc97e2cf1e3909e7485c8d1b01dcf765f
Felipe de Mello <13694949+fdemello@users.noreply.github.com>
2018-12-22 16:21:26 -0500

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     name=PACKAGE_NAME,
     author='Felipe de Mello',
     author_email='fdemello@cisco.com',
-    version='0.5.0',
+    version='0.6.0',
     url='https://github.com/CiscoDevNet/webex-teams-archiver',
     description='Room archiver utility for Webex Teams',
     long_description=long_description,
